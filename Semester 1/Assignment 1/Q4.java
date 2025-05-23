5d3d295d7b15165acff2719be7aa7eab395d5fde
Prince Patra <princepatra9951@gmail.com>
2025-05-23 20:32:20 +0530

    Create Q4.java

---
+++ Semester 1/Assignment 1/Q4.java
@@ -0,0 +1,19 @@
+package Assignment1;
+
+public class Q04 {//Write a java program that will display you First , Middle ,Last name in different lines without using the 
+	//Method Println()
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String First ="Prince";
+		String Middle ="";
+		String Last="Patra";
+		System.out.print(First +"\n"+Middle+"\n"+Last);
+		
+	}
+
+}
+// output:
+// Prince
+
+// Patra
