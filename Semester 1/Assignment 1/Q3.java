5319fd8e94f1881ff68bdd6b98172632b38a268b
Prince Patra <princepatra9951@gmail.com>
2025-05-23 20:27:59 +0530

    Create Q3.java

---
+++ Semester 1/Assignment 1/Q3.java
@@ -0,0 +1,25 @@
+package Assignment1;
+
+public class Q03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("##########################################");
+		System.out.println("##\t\t\t\t\t##");
+		System.out.println("##\t\t\t\t\t##");
+		System.out.println("##\t\t\t\t\t##");
+		System.out.println("##\t\t\t\t\t##");
+		System.out.println("##\t\t\t\t\t##");
+		System.out.println("##########################################");
+	}
+
+}
+
+// output:
+// ##########################################
+// ##                             					##
+// ##                             					##
+// ##                             					##
+// ##                             					##
+// ##                             					##
+// ##########################################
