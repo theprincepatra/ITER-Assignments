567ca9d7f923869ba2d4d2fcbefe0f0e3f544f4c
Prince Patra <princepatra9951@gmail.com>
2025-05-26 21:47:00 +0530

    Update Q07.java

--- Semester 1/Assignment 1/Q07.java
+++ Semester 1/Assignment 1/Q07.java
@@ -4,18 +4,18 @@ public class Q07 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			int x= 123 ;
-			int y = 456;
-			System.out.println("X = "+x+"\nY = "+y);
+			int x=10;
+			int y =20;
+			System.out.println("X = "+x+"\nY= "+y);
 			int z=x;
 			x=y;
 			y=z;
-			System.out.println("X = "+x+"\nY = "+y);
+			System.out.println("X = "+x+"\nY= "+y);
 	}
 
 }
 // output:
-// X = 123
-// Y = 456
-// X = 456
-// Y = 123
+// X = 10
+// Y = 20
+// X = 20
+// Y = 10
