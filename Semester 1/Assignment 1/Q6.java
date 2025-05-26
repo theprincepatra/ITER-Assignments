eb66434f9a6c8fe831add1eb9516369346a35f91
Prince Patra <princepatra9951@gmail.com>
2025-05-26 21:21:01 +0530

    Create Q6.java

---
+++ Semester 1/Assignment 1/Q6.java
@@ -0,0 +1,13 @@
+package Assignment1;
+
+public class Q06 {
+	public static void main(String args[]) {
+		int an = 111222333;
+		String name = "Prince Patra";
+		double balance = 9999.99;
+		System.out.println("\"My name is "+name+" bearing account number " +an+ " having balance "+balance+"\"");
+	}
+
+}
+// output:
+// "My name is Prince Patra bearing account number 111222333 having balance 9999.99"
