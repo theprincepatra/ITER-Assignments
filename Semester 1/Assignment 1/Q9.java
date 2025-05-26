f4ea5acc13989900a3956575ed41cc2fa99120ff
Prince Patra <princepatra9951@gmail.com>
2025-05-26 21:27:21 +0530

    Create Q9.java

---
+++ Semester 1/Assignment 1/Q9.java
@@ -0,0 +1,26 @@
+package Assignment1;
+
+public class Q09 {
+
+    public static void main(String[] args) {
+        System.out.println(8);
+        System.out.println((float)30 / 4);
+        System.out.println(3 + '4');
+        System.out.println(2 + 5 + '7');
+        System.out.println("85" + 3 + 9);
+        System.out.println(7 + "2");
+        System.out.println('*' * 20);
+        System.out.println('a');
+        System.out.println('x' + 'y');
+    }
+}
+// output:
+// 8
+// 7.5
+// 55
+// 56
+// 8539
+// 72
+// 1600
+// a
+// 241
