a5890106ac8b42af5d683c20efdd5cc03fa580f2
Prince Patra <princepatra9951@gmail.com>
2025-05-26 21:24:45 +0530

    Create Q8.java

---
+++ Semester 1/Assignment 1/Q8.java
@@ -0,0 +1,24 @@
+package Assignment1;
+
+public class Q08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y =20;
+		System.out.println("X = "+x+"\nY= "+y);
+		x=x+y;
+		y=x-y;
+		x=x-y;
+		System.out.println("X = "+x+"\nY= "+y);
+		
+		
+
+	}
+
+}
+// output:
+// X = 123
+// Y= 456
+// X = 456
+// Y= 123
