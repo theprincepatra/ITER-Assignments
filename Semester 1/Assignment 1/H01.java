4fb79641d23e5a9212f30a9903db7eeb7cf0a501
Prince Patra <princepatra9951@gmail.com>
2025-05-30 13:55:39 +0530

    Create H01.java

---
+++ Semester 1/Assignment 1/H01.java
@@ -0,0 +1,20 @@
+package Assignment1;
+
+public class HQ1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println(2+"bc");
+			System.out.println(2+3+"bc");
+			System.out.println((2+3)+"bc");
+			System.out.println("bc"+(2+3));
+			System.out.println("bc"+2+3);
+	}
+
+}
+// output:
+// 2bc  
+// 5bc  
+// 5bc  
+// bc5  
+// bc23
