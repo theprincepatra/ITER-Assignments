cfad02e25846863f1a49a26a26ba125c97703a87
Prince Patra <princepatra9951@gmail.com>
2025-05-30 14:00:09 +0530

    Update Q1.java

--- Semester 1/Assignment 2/Q1.java
+++ Semester 1/Assignment 2/Q1.java
@@ -1,1 +1,15 @@
+package Assignment2;
+import java.util.Scanner;
+public class Q01{
+	public static void main(String[] args) {
+		System.out.print("Enter Temperature in Fahrenheit  ");
+		Scanner sc =new Scanner(System.in);
+		double f= sc.nextDouble();
+		double c=(f-32)*5.0/9;
+		System.out.println(f+" Fahrenheit is "+c+" Celsius ");
+		sc.close();
+	}
 
+}
+// output:
+// Enter Temperature in Fahrenheit  98.6 Fahrenheit is 37.0 Celsius 
