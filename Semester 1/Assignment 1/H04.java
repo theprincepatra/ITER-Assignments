10b87a3bb1cdea53be87ec0dbdce160947eaba36
Prince Patra <princepatra9951@gmail.com>
2025-05-30 13:58:38 +0530

    Create H04.java

---
+++ Semester 1/Assignment 1/H04.java
@@ -0,0 +1,26 @@
+package Assignment1;
+
+public class HQ4 {
+
+	public static void main(String[] args) {
+		
+		double a =3.14159;// TODO Auto-generated method stub
+		System.out.println(a);
+		System.out.println(a+1);
+		System.out.println(8/(int)a);
+		System.out.println(8/a);
+		System.out.println((8/a));
+		System.out.println();
+		short b= 32767;
+		System.out.println((short) b+2);
+    
+	}
+}
+// output:
+// 3.14159  
+// 4.14159  
+// 2  
+// 2.546481240981106  
+// 2.546481240981106  
+
+// 32769
