d85bc10e87304f90f25eb2e8c68c9cfbee16c724
Prince Patra <princepatra9951@gmail.com>
2025-11-10 19:57:23 +0530

    Create q03.py

---
+++ Semester 3/CSW/ASSIGNMENT 2/q03.py
@@ -0,0 +1,38 @@
+# Q3: Stack using list
+
+stack = []
+
+def push(x):
+    stack.append(x)
+
+def pop():
+    if not stack:
+        print("Stack is empty!")
+    else:
+        print("Popped:", stack.pop())
+
+def display():
+    print("Stack:", stack)
+
+def evaluate_rpn(expr):
+    items = expr.split()
+    temp = []
+    for i in items:
+        if i.isdigit():
+            temp.append(int(i))
+        else:
+            b = temp.pop()
+            a = temp.pop()
+            if i == '+': temp.append(a + b)
+            elif i == '-': temp.append(a - b)
+            elif i == '*': temp.append(a * b)
+            elif i == '/': temp.append(a / b)
+    print("Result:", temp[0])
+
+# Example usage:
+push(40)
+push(20)
+display()
+pop()
+display()
+evaluate_rpn("5 3 4 * +")
