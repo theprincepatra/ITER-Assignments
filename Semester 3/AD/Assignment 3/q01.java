fffdf4287e2abb9938e3978e138043c9a402da50
Prince Patra <princepatra9951@gmail.com>
2025-11-11 10:05:58 +0530

    Create q01.java

---
+++ Semester 3/AD/Assignment 3/q01.java
@@ -0,0 +1,32 @@
+/*
+1. Selection Sort
+   Problem: Sort an array using the selection sort algorithm.
+*/
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+        System.out.print("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
+/*
+Output:
+Sorted array: 11 12 22 25 64
+*/
