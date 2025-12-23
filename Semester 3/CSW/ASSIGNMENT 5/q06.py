272ae5eb876f2b3a2979f4073c9d55a18d500a86
Prince Patra <princepatra9951@gmail.com>
2025-12-23 10:05:09 +0530

    Implement diary file creation and reading
    Added functionality to create and read a diary file.

--- Semester 3/CSW/ASSIGNMENT 5/q06.py
+++ Semester 3/CSW/ASSIGNMENT 5/q06.py
@@ -1,8 +1,18 @@
+# a)
 import os,datetime
 if os.path.exists("diary.txt"):print("File already exists")
 else:
     with open("diary.txt","w") as f:
         f.write(str(datetime.date.today())+"\n"+input())
-
 # Output:
 # Enter diary note:
+
+# b)
+try:
+    with open("diary.txt") as f:print(f.read())
+except FileNotFoundError:
+    print("File not found. Please check the name.")
+
+# Output:
+# 2025-12-23
+# Today was a good day.
